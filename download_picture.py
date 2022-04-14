e338f3dd7ad7c61021fb9e2684e7fe0732c8ac00
MSMikl <michalbl4@gmail.com>
2022-04-14 15:03:28 +0300

    Little "code-drying" correction

--- download_picture.py
+++ download_picture.py
@@ -8,7 +8,6 @@ def download_picture(url, path):
     Path(path).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     if response.ok:
-        path = Path(path, os.path.basename(url))
-        with open(path, 'wb') as file:
+        with open(Path(path, os.path.basename(url)), 'wb') as file:
             file.write(response.content)
     return
