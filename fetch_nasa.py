e41c9ed8752217e582136f0954872b929a8f15a4
MSMikl <michalbl4@gmail.com>
2022-04-16 14:28:03 +0300

    Code formatted

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -8,7 +8,8 @@ def fetch_nasa_apod(token, count=5, download=True):
     params = {'api_key': token, 'count': count}
     response = requests.get(
         'https://api.nasa.gov/planetary/apod',
-        params=params)
+        params=params
+    )
     result_links = []
     for picture in response.json():
         if picture['hdurl']:
@@ -16,7 +17,10 @@ def fetch_nasa_apod(token, count=5, download=True):
                 try:
                     download_picture(picture['hdurl'], './images/NASA/')
                 except requests.exceptions.ConnectionError:
-                    print('Невозможно скачать картинку по ссылке', picture['hdurl'])
+                    print(
+                        'Невозможно скачать картинку по ссылке',
+                        picture['hdurl']
+                    )
                     continue
                 except requests.exceptions.MissingSchema:
                     print(picture['hdurl'], 'не является ссылкой')
@@ -29,16 +33,21 @@ def fetch_nasa_epic(token, count=5, download=True):
     params = {'api_key': token}
     response = requests.get(
         'https://epic.gsfc.nasa.gov/api/natural',
-        params=params)
+        params=params
+    )
     result_links = []
     for picture in response.json():
         if count:
-            date = datetime.datetime.strptime(picture['identifier'], '%Y%m%d%f')
+            date = datetime.datetime.strptime(
+                picture['identifier'],
+                '%Y%m%d%f'
+            )
             url = 'https://epic.gsfc.nasa.gov/archive/natural/{}/{:02d}/{:02d}/png/{}.png'.format(
                 date.year,
                 date.month,
                 date.day,
-                picture['image'])
+                picture['image']
+            )
             if download:
                 download_picture(url, './images/NASA/EPIC/')
             count -= 1
